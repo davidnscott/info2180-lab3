49f28f359c031f784c9396867e861f47ab6d28d3
David Scott <davidnscott.ja@gmail.com>
2020-10-11 21:28:21 -0500

    Added win conditions

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,20 @@
 window.onload=function(){
+	const winPerms = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 	let gameState = ["","","","","","","","",""];
 	var turn = "X";
 	let board = document.querySelectorAll("#board div");
+	let message = document.querySelector("#status");
 	for (let x=0; x<9;x++){
 		board[x].classList.add("square");
 		board[x].addEventListener('click', function(){
 			board[x].textContent = turn;
 			gameState[x] = turn;
+			for (y = 0; y < winPerms.length; y++){
+				if (gameState[winPerms[y][0]] == turn && gameState[winPerms[y][1]] == turn && gameState[winPerms[y][2]] == turn){
+					message.textContent = "Congratulations! " + turn + " is the winner";
+					message.classList.add("you-won");
+				}
+			}
 			if (turn == 'X'){
 				board[x].classList.add("X");
 				turn = 'O';
