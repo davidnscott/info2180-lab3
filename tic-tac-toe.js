55210d3b4a9bcbf494522fafbabad6b82eb933bf
David Scott <davidnscott.ja@gmail.com>
2020-10-11 20:57:22 -0500

    Turns introduced.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,20 @@
 window.onload=function(){
+	let gameState = ["","","","","","","","",""];
+	var turn = "X";
 	let board = document.querySelectorAll("#board div");
 	for (let x=0; x<9;x++){
-		board[x].classList.add("square");			
+		board[x].classList.add("square");
+		board[x].addEventListener('click', function(){
+			board[x].textContent = turn;
+			gameState[x] = turn;
+			if (turn == 'X'){
+				board[x].classList.add("X");
+				turn = 'O';
+			}else{
+				board[x].classList.add("O");
+				turn = 'X'
+			};
+		});
+			
 	};		
 };
-
