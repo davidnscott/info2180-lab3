07a6b189a64ae61b764af59f08a4db4f7ecde1f4
David Scott <davidnscott.ja@gmail.com>
2020-10-11 23:43:13 -0500

    Added reset button

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,21 @@
-window.onload=function(){
+window.onload=function start(){
 	const winPerms = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 	let gameState = ["","","","","","","","",""];
 	var turn = "X";
 	let board = document.querySelectorAll("#board div");
 	let message = document.querySelector("#status");
+	let resetButton = document.querySelector(".btn");
+	resetButton.addEventListener('click', function(){
+		message.classList.remove("you-won");
+		message.textContent = "Move your mouse over a square and click to play an X or an O.";
+		gameState = ["","","","","","","","",""];
+		turn = "X";
+		for (let i=0; i<9;i++){
+			board[i].textContent = "";
+			board[i].classList.remove("X");
+			board[i].classList.remove("O");			
+		}			
+	});
 	for (let x=0; x<9;x++){
 		board[x].classList.add("square");
 		board[x].addEventListener('click', function(){
